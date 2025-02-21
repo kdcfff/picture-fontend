40c94f74bb1891ed74f32b62f8ca8d472550e357
一路向北 <2480155857@qq.com>
2025-02-21 10:42:45 +0800

    feat(layout): 重构基本布局并添加全局头部组件
    - 重新设计了 BasicLayout 组件的结构，添加了 GlobalHeader 组件- 更新了布局样式，包括头部、内容和底部的样式
    - 在头部添加了 logo 和导航菜单
    - 优化了底部样式，增加了背景色和对齐方式

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="globalHeader">
+    <a-row>
+      <a-col flex="100px">
+        <router-link to="/">
+          <div class="logo">
+            <img src="../assets/logo.jpg" alt="logo">
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+      </a-col>
+      <a-col>
+        <a-button type="primary">登录</a-button>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+
+const current = ref<string[]>(['mail'])
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: 'Navigation One',
+    title: 'Navigation One'
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于'
+  },
+  {
+    key: '/pay',
+    label: '充值',
+    title: '充值'
+  }
+])
+</script>
+<style scoped>
+#globalHeader {
+  align-items: center;
+  background-color: #fff;
+}
+
+.logo {
+  align-items: center;
+  height: 100%;
+}
+</style>
+
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,19 +1,44 @@
 <script setup lang="ts">
 
+import GlobalHeader from '@/components/GlobalHeader.vue'
 </script>
 
 <template>
   <div id="basicLayout">
     <a-layout style="min-height: 100vh">
-      <a-layout-header>Header</a-layout-header>
-      <a-layout-content>Content</a-layout-content>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
       <a-layout-footer class="footer">
-        <a href="https://github.com/vuejs/theme">vuejs/theme</a>
+        <a href="https://github.com/vuejs/theme" target="_blank">vuejs</a>
       </a-layout-footer>
     </a-layout>
   </div>
 </template>
 
 <style scoped>
+#basicLayout .header {
+  padding-inline: 10px;
+  background: white;
+}
 
+#basicLayout .footer {
+  background: #efefef;
+  padding: 20px;
+  font-size: large;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+#basicLayout .content {
+  padding: 20px 20px 20px 40px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 50px;
+}
 </style>
